b7aeded3cd8d217b0a30c3e676e019b75f6249f9
Luther History <whoismrhistory@gmail.com>
2026-09-15 13:29:12 +0630

    fix: done to polish v0.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 // C standard headers
+#include <cstdlib>
 #include <cstring>
 #include <cctype>
 
@@ -10,8 +11,34 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+auto tokenizer(char* data) {
+	std::vector<char*> args;
+
+	char *token = std::strtok(data, " ");
+
+	while (token != nullptr) {
+		args.push_back(token);
+		
+		if (std::strcmp(args[0], "exit") == 0) {
+
+			exit(0);
+		}
+
+		token = std::strtok(nullptr, " ");
+	}
+	args.push_back(nullptr);
+
+	return args;
+}
+
+// auto handle_redirect(std::vector<char*>& args)
+// {
+
+// 	return 0;
+// }
+
 bool can_parse(std::string& buf)
-{
+{	
 	for (unsigned char c : buf) {
 		if (!std::isspace(c))
 			return true;
@@ -22,38 +49,20 @@ bool can_parse(std::string& buf)
 
 void process(std::string& buf, int& status)
 {
-	char *token = std::strtok(buf.data(), " ");
+	auto args 	= tokenizer(buf.data());		
+	auto pid 	= fork();
 
-	if (std::strcmp(token, "exit") == 0) {
-		exit(0);
-	}
-
-	pid_t pid 	= fork();
 
 	if (pid < 0) 
 	{
 		// I'm the failure of forking. (Something weird though btw >D)
-		std::cerr << "Failed to fork" << std::endl;
-		return;
+		std::cerr << "Failed to load" << std::endl;
+		exit(EXIT_FAILURE);
 	}
 
 	else if (pid == 0) 
 	{
 		// I'm the child.
-		std::vector<char*> args;		
-
-		while (token != nullptr) {
-			args.push_back(token);
-
-			token = std::strtok(nullptr, " ");
-		}
-
-		(args.empty())
-		?
-			_exit(0)
-		:
-			args.push_back(nullptr);
-
 		if (execvp(args[0], args.data())) {
 			std::cerr 
 				<< "mish: Unknown command: " 
@@ -73,11 +82,13 @@ void process(std::string& buf, int& status)
 
 int main(int argc, char **argv)
 {
+	if (argc < 1) (void) argc, (void) argv;
+
 	int status = 0;
 
 	while (true){
 		std::string buf;
-	
+		
 		std::cout << "$ ";
 		std::getline(std::cin, buf, '\n');
 	
